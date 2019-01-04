bfea983148066e47c5820e94404c96f61021299d
Luciano Sclovsky <luciano.sclovsky@softdesign-rs.com.br>
2019-01-04 20:02:10 -0200

    Added initial tableView

--- HabitTracker/ViewController.swift
+++ HabitTracker/ViewController.swift
@@ -12,9 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
 
 }
 
+
+extension ViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weekCell", for: indexPath)
+        return cell
+    }
+    
+}
