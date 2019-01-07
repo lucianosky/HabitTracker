7629978e0d5c238adfa99ceed253e02fe3c18d74
Luciano Sclovsky <luciano.sclovsky@softdesign-rs.com.br>
2019-01-06 22:43:51 -0200

    Added month name and navigation buttons

--- HabitTracker/CalMonth.swift
+++ HabitTracker/CalMonth.swift
@@ -18,25 +18,41 @@ struct CalMonth {
 
     init() {
         var components = Calendar.current.dateComponents([.year, .month], from: Date())
+        // TODO optionals
+        self.init(year: components.year!, month: components.month!)
+    }
+    
+    init (date: Date) {
+        var components = Calendar.current.dateComponents([.year, .month], from: date)
+        // TODO optionals
+        self.init(year: components.year!, month: components.month!)
+    }
+    
+    init(year: Int, month: Int) {
+        var components = DateComponents()
+        components.year = year
+        components.month = month
         components.day = 1
         components.hour = 0
         components.minute = 0
         components.second = 0
-    
+        
         firstMonthDate = Calendar.current.date(from: components)!
         lastMonthDate = Calendar.current.date(byAdding: DateComponents(month: 1, day: -1), to: firstMonthDate)!
-
+        
         let firstWeekday = Calendar.current.component(.weekday, from: firstMonthDate)
         let lastWeekday = Calendar.current.component(.weekday, from: lastMonthDate)
+        // TODO: this works for weeks starting MONDAY
         let deltaBefore = firstWeekday == 1 ? -6 : -firstWeekday + 2
         let deltaAfter = lastWeekday == 1 ? 0 : 8 - lastWeekday
         
         firstCalDate = Calendar.current.date(byAdding: DateComponents(day: deltaBefore), to: firstMonthDate)!
         lastCalDate = Calendar.current.date(byAdding: DateComponents(day: deltaAfter), to: lastMonthDate)!
-
+        
         let days = Calendar.current.dateComponents([.day], from: firstCalDate, to: lastCalDate).day! + 1
         weeks = days / 7
     }
+    
     
     func getWeekDays(_ week: Int) -> [CalDay] {
         // TODO: optionals
@@ -50,6 +66,34 @@ struct CalMonth {
             date = Calendar.current.date(byAdding: DateComponents(day: 1), to: date)!
         }
         return calDays
+    }
+    
+    func monthName() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "LLLL"
+        return dateFormatter.string(from: firstMonthDate)
+    }
+    
+    func year() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY"
+        return dateFormatter.string(from: firstMonthDate)
+    }
+    
+    func prevMonth() -> Date {
+        // TODO optional
+        return Calendar.current.date(byAdding: DateComponents(month: -1), to: firstMonthDate)!
+    }
+    
+    func nextMonth() -> Date {
+        // TODO optional
+        return Calendar.current.date(byAdding: DateComponents(month: 1), to: firstMonthDate)!
+    }
+    
+    static func getWeekHeaders() -> [String] {
+        // TODO: this works for weeks starting MONDAY
+        return [ "M", "T", "W", "T", "F", "S", "S" ]
+        //return Calendar.current.veryShortWeekdaySymbols
     }
     
 }
--- HabitTracker/ViewController.swift
+++ HabitTracker/ViewController.swift
@@ -11,29 +11,68 @@ import UIKit
 class ViewController: UIViewController {
     
     var calMonth: CalMonth?
-
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         calMonth = CalMonth()
+        refreshData()
     }
-
+    
+    @IBAction func prevMonthTouched(_ sender: Any) {
+        // TODO optional
+        calMonth = CalMonth(date: calMonth!.prevMonth())
+        tableView.reloadData()
+        refreshData()
+    }
+    
+    @IBAction func nextMonthTouched(_ sender: Any) {
+        // TODO optional
+        calMonth = CalMonth(date: calMonth!.nextMonth())
+        tableView.reloadData()
+        refreshData()
+    }
+    
+    func refreshData() {
+        monthLabel.text = calMonth?.monthName()
+        yearLabel.text = calMonth?.year()
+    }
+    
 }
 
 
 extension ViewController: UITableViewDataSource {
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return calMonth?.weeks ?? 0
+        return section == 0 ? 1 : calMonth?.weeks ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "weekCell", for: indexPath)
-        if let days = calMonth?.getWeekDays(indexPath.row) {
+        if indexPath.section == 0 {
+            let headers = CalMonth.getWeekHeaders()
             for tag in 0...6 {
                 if let button = cell.viewWithTag(tag+1) as? UIButton {
-                    button.setTitle("\(days[tag].day)", for: .normal)
-                    let color = days[tag].fromMonth ? UIColor.black : UIColor.gray
-                    button.setTitleColor(color, for: .normal)
+                    button.setTitle("\(headers[tag])", for: .normal)
+                    button.setTitleColor(UIColor.gray, for: .normal)
+                    button.isEnabled = false
+                }
+            }
+        } else {
+            if let days = calMonth?.getWeekDays(indexPath.row) {
+                for tag in 0...6 {
+                    if let button = cell.viewWithTag(tag+1) as? UIButton {
+                        let color = days[tag].fromMonth ? UIColor.black : UIColor.gray
+                        button.setTitle("\(days[tag].day)", for: .normal)
+                        button.setTitleColor(color, for: .normal)
+                        button.isEnabled = days[tag].fromMonth
+                    }
                 }
             }
         }
